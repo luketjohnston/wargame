62b6def712fcca56ba1bb71cf1019da871022941
Luke Johnston <luketjohnston32@gmail.com>
2020-06-04 16:10:06 -0700

    asdjk

--- wargame/settings.py
+++ wargame/settings.py
@@ -133,9 +133,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+    #os.path.join(BASE_DIR, 'static'),
+#)
 
 ASGI_APPLICATION = "wargame.routing.application"
 
