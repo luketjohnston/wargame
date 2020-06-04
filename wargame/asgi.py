897a76d14d50f66147da3ad29eef284f2ec06e96
Luke Johnston <luketjohnston32@gmail.com>
2020-06-04 16:59:36 -0700

    trying to reduce connections

--- wargame/asgi.py
+++ wargame/asgi.py
@@ -14,5 +14,7 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wargame.settings')
+os.environ['ASGI_THREADS'] = "4"
 django.setup()
 application = get_default_application()
+
