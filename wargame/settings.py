d26e6b4580c6906bea1a967b13e252d4939535fb
Luke Johnston <luketjohnston32@gmail.com>
2020-06-04 11:54:45 -0700

    gamelist page and heroku deployment

--- wargame/settings.py
+++ wargame/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 
 ASGI_APPLICATION = "wargame.routing.application"
 
@@ -131,3 +134,6 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+
+django_heroku.settings(locals())
